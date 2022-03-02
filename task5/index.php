428b7a3ad8171c2c7755407fb3363fd83a3d392a
Anna <anna.ololoshka@yandex.ru>
2022-03-02 21:43:25 +0300

    new

---
+++ task5/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$content = renderTemplate("about", 1234);
+$menu=renderTemplate("menu");
+$title=renderTemplate("title");
+
+echo renderTemplate("layout", $content,$menu,$title);
+
+
+function renderTemplate($page, $content = '', $menu='', $title='') {
+    ob_start();
+    include $page . ".php";
+    return  ob_get_clean();
+}
+
+//out = "
+//bufer =""
